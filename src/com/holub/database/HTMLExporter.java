91e6320df47962ed61a62fa0460622f45b152533
hnnynh <hnnynh125@gmail.com>
2023-11-13 23:42:11 +0900

    fix: add \n in HTMLExporter

--- src/com/holub/database/HTMLExporter.java
+++ src/com/holub/database/HTMLExporter.java
@@ -11,46 +11,46 @@ public class HTMLExporter implements Table.Exporter {
     }
 
     public void startTable() throws IOException {
-        out.write("<html>");
-        out.write("<body>");
+        out.write("<html>\n");
+        out.write("<body>\n");
     }
 	
     public void storeMetadata(String tableName, int width, int height, Iterator columnNames) throws IOException {
-    	out.write("<table>");
+    	out.write("<table>\n");
     	out.write("<caption>");
     	out.write(tableName == null ? "anonymous" : tableName);		// title of the table
     	out.write("</caption>");
    
-    	out.write("<thead>");
-    	out.write("<tr>");
+    	out.write("<thead>\n");
+    	out.write("<tr>\n");
     	
     	while(columnNames.hasNext()) {
     		out.write("<th scope=\"col\">");
     		out.write(columnNames.next().toString());
-            out.write("</th>");
+            out.write("</th>\n");
     	}
     	
-    	out.write("</tr>");
-    	out.write("</thead>");
-    	out.write("<tbody>");
+    	out.write("</tr>\n");
+    	out.write("</thead>\n");
+    	out.write("<tbody>\n");
     }
 
     public void storeRow(Iterator data) throws IOException {
-    	out.write("<tr>");
+    	out.write("<tr>\n");
     	while(data.hasNext()) {
     		out.write("<td>");
     		out.write(data.next().toString());
-            out.write("</td>");
+            out.write("</td>\n");
     	}
-    	out.write("</tr>");
+    	out.write("</tr>\n");
     }
 
 
     public void endTable() throws IOException {
-    	out.write("</tbody>");
-        out.write("</table>");
-        out.write("</body>");
-        out.write("</html>");
+    	out.write("</tbody>\n");
+        out.write("</table>\n");
+        out.write("</body>\n");
+        out.write("</html>\n");
 
         out.close();
     }
