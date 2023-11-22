35ff9c700d0e5ca0c05d627002ded43079d1dd50
bbx8216 <bbx8216@gmail.com>
2023-11-22 20:43:49 +0900

    feat: 구현 예정 set 함수들 삽입

--- src/com/holub/database/jdbc/JDBCPreparedStatement.java
+++ src/com/holub/database/jdbc/JDBCPreparedStatement.java
@@ -25,4 +25,30 @@ public class JDBCPreparedStatement extends JDBCStatement{
     protected static PreparedStatement getInstance(JDBCConnection conn, String sql, Database db) throws SQLException {
         return new JDBCPreparedStatement(conn, sql, db);
     }
+
+    public void setInt(int parameterIndex, int x) {
+        ((PreparedQuery) this.query).getQueryBindings().setInt(getCoreParameterIndex(parameterIndex), x);
+    }
+
+    public void setFloat(int parameterIndex, float x) throws SQLException {
+        ((PreparedQuery) this.query).getQueryBindings().setFloat(getCoreParameterIndex(parameterIndex), x);
+    }
+
+    public void setDouble(int parameterIndex, double x) throws SQLException {
+        synchronized (checkClosed().getConnectionMutex()) {
+            ((PreparedQuery) this.query).getQueryBindings().setDouble(getCoreParameterIndex(parameterIndex), x);
+        }
+    }
+
+    public void setLong(int parameterIndex, long x) throws SQLException {
+        synchronized (checkClosed().getConnectionMutex()) {
+            ((PreparedQuery) this.query).getQueryBindings().setLong(getCoreParameterIndex(parameterIndex), x);
+        }
+    }
+
+    public void setString(int parameterIndex, String x) throws SQLException {
+        synchronized (checkClosed().getConnectionMutex()) {
+            ((PreparedQuery) this.query).getQueryBindings().setString(getCoreParameterIndex(parameterIndex), x);
+        }
+    }
 }
